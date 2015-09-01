29b4ba1a8e60c138147ce2c13724e217895a71b7
takurou Matsuo <m.taku7@gmail.com>
2015-09-01 15:47:23 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BarOpeningHoursAizu::Application.config.secret_key_base = '219218469e7f0b79731c77d408277fa62760ee760ffd7340aad496bfa9769a4df5c2416e9a4addc48cd509c18b4a74610918fe97d22a377935f09a3871bffd4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BarOpeningHoursAizu::Application.config.session_store :cookie_store, key: '_BarOpeningHoursAizu_session'
