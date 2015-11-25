46e5115609c781cd7791467ce584541a09c0b24d
takurou Matsuo <m.taku7@gmail.com>
2015-11-25 13:41:13 +0900

    営業時間表示完了

--- app/helpers/shopinfos_helper.rb
+++ app/helpers/shopinfos_helper.rb
@@ -5,15 +5,25 @@ module ShopinfosHelper
     end_hour = week_time[4] + week_time[5]
     end_minute = week_time[6] + week_time[7]
 
-    now_time = Time.now
+    now_time = Time.now + 9.hour
+
     now_year = now_time.strftime("%Y")
     now_month = now_time.strftime("%m")
     now_day = now_time.strftime("%d")
     now_week = now_time.strftime("%A")
-    now_time = now.strftime("%X")
+    now_time = now_time.strftime("%X")
     today_start_time = Time.mktime(now_year,now_month,now_day,start_hour,start_minute,00)
     today_end_time = Time.mktime(now_year,now_month,now_day,end_hour,end_minute,00)
-    return today_start_time　today_end_time
+
+
+    if now_time > today_start_time
+      # 営業はスタートしている
+      if today_end_time > now_time
+        # 営業中
+        return today_start_time,today_end_time
+      end
+    end
+
   end
   def make_now_week
     now_time = Time.now
